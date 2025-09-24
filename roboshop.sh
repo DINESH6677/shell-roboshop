b251f38f8679fc65e413fc3b5e9433bc564c0b12
DINESH6677 <dgadila@my.bridgeport.edu>
2025-09-23 20:22:10 -0400

    roboshop_practice

--- roboshop.sh
+++ roboshop.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 AMI_ID=ami-09c813fb71547fc4f
 SG_ID=sg-013a9c377a5c2354d
+ZONE_ID=Z09655201PB8BBQ9OOULW
+DOMAIN_NAME="devopswithdinesh.com"
 
 for instance in $@
 do
@@ -8,9 +10,28 @@ do
 
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids  $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        RECORD_NAME=$instance.$DOMAIN_NAME
     else
         IP=$(aws ec2 describe-instances --instance-ids  $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        RECORD_NAME=$DOMAIN_NAME
     fi
     echo "$instance : $IP"
+
+    aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch '
+  {
+    "Comment": "Update a record set",
+    "Changes": [{
+      "Action": "UPSERT",
+      "ResourceRecordSet": {
+        "Name": "'$RECORD_NAME'",
+        "Type": "A",
+        "TTL": 1,
+        "ResourceRecords": [{ "Value": "'$IP'" }]
+      }
+    }]
+  }'
+
 
 done
